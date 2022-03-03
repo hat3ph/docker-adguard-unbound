d94553d0f62e2a397d69bcc84db4f23fa1f28f22
hat3ph <88069788+hat3ph@users.noreply.github.com>
2022-03-03 15:23:03 +0800

    Create disable_dnsstublistener.sh

---
+++ disable_dnsstublistener.sh
@@ -0,0 +1,21 @@
+#/usr/bash
+
+SERVICE=systemd-resolved.service
+PORT=53
+
+if (systemctl -q is-active $SERVICE)
+    then
+    echo "Application $SERVICE is running."
+        if (ss -tunlp | grep $PORT > /dev/null)
+                then
+                echo "Network port $PORT is running."
+                mkdir -p /etc/systemd/resolved.conf.d/
+                printf "[Resolve]\nDNS=127.0.0.1\nDNSStubListener=no" > /etc/systemd/resolved.conf.d/adguardhome.conf
+                mv /etc/resolv.conf /etc/resolv.conf.backup
+                ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+				echo "Restarting $SERVICE"
+                systemctl restart $SERVICE
+                exit 1
+        fi
+    exit 1
+fi
